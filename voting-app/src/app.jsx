598a5cbedbc9ae6379a2e9ee08258af158f49e64
Daniel Sampaio Pereira <daniel.pereira.vr@gmail.com>
2025-03-20 17:14:56 +0000

    primiera parte do exercicio1
    até aos 16 mins

---
+++ voting-app/src/app.jsx
@@ -0,0 +1,13 @@
+import { ProductsList } from './components/products-list'
+
+export function App() {
+    return (
+        <div>
+            <h1>Voting App</h1>
+            <hr />
+
+            <ProductsList />
+        </div>
+        
+    )
+}
